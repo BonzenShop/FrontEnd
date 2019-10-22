3fa5dd70b7ce1cad069cd06e3b37cedb43c10691
DanielWunder <41953388+DanielWunder@users.noreply.github.com>
2019-10-22 11:10:24 +0200

    implemented filter function for product list and activated category button in top bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { HttpClientModule } from '@angular/common/http';
- 
+
+import { ItemCategoryFilterPipe } from './shared/item-category-filter.pipe';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -19,7 +20,8 @@ import { HomeComponent } from './home/home.component';
     AppComponent,
     ProductListComponent,
     ProductDetailComponent,
-    HomeComponent
+    HomeComponent,
+    ItemCategoryFilterPipe
   ],
   imports: [
     BrowserModule,
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
 import { Item } from '../item';
 import { ApiService } from '../api.service';
 
@@ -10,12 +11,16 @@ import { ApiService } from '../api.service';
 export class ProductListComponent implements OnInit {
   
   productList: Item[]
+  category: String
 
-  constructor(private apiService: ApiService) { }
+  constructor(private apiService: ApiService, private route: ActivatedRoute) { 
+    this.route.queryParams.subscribe(params => {
+      this.category = params['cat'];
+  });
+  }
 
   ngOnInit() {
     this.apiService.getProducts().subscribe((data)=>{
-      console.log(data);
       this.productList = data;
     })
   }
